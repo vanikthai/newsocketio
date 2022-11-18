f09fcfc7b2f35a5f2ead1c78deda00f10ef2a9b9
vanikthai <vanikthai@gmail.com>
2022-11-18 15:54:17 +0700

    u

--- index.js
+++ index.js
@@ -3,20 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  serveClient: true,
-  origins: "*:*",
-  transports: ["polling"],
-  pingInterval: 60000,
-  pingTimeout: 25000,
-  cookie: true,
-  cors: {
-    origin: "http://localhost:300",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["vanikthaiapp"],
-    credentials: true,
-  },
-});
+const io = new Server(server);
 const layout = require("express-ejs-layouts");
 const part = require("path");
 app.set("view engine", "ejs");
@@ -27,7 +14,11 @@ app.use(require("./routes"));
 io.on("connection", (socket) => {
   socket.on("message", (msg) => {
     console.log(msg);
-    io.emit("message", msg);
+    let payload = {
+      id: socket.id,
+      ...msg,
+    };
+    io.emit("message", payload);
   });
 });
 
